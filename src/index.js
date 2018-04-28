05aa0ba8d32929e741d1d4cce724e4dfd402453d
laveesingh <lavees96@gmail.com>
2018-04-28 12:52:32 +0530

    added statefulness to App component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import City from './City'
 import Activity from './Activity'
 import './styles/index.css'
 
-const App = props => (
-  <BrowserRouter>
-    <Switch>
-      <Route path='/city/:city' render={
-        props => <City {...props} />
-      }/>
-      <Route path='/activity/<activity>' render={
-        props => <Activity {...props} />
-      }/>
-    </Switch>
-  </BrowserRouter>
-)
+class App extends React.Component {
+  state = {}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  setData = data => this.setState({...data})
 
+  render() {
+    const childrenProps = {
+      setData: this.setData,
+      ...this.state,
+    }
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route
+            path="/city/:city"
+            render={props => <City {...props} {...childrenProps} />}
+          />
+          <Route
+            path="/activity/<activity>"
+            render={props => <Activity {...props} {...childrenProps} />}
+          />
+        </Switch>
+      </BrowserRouter>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
