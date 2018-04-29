0e82a57ac4c7a81a40b4cca500668db88b437cc7
laveesingh <lavees96@gmail.com>
2018-04-29 12:38:38 +0530

    added redirection on activity click

--- src/City.js
+++ src/City.js
@@ -20,24 +20,43 @@ export default class City extends React.Component {
     this.fetchCityBlob()
   }
 
+  handleActivityClick = (event, activityId) => {
+    this.props.history.push(`/activity/${activityId}`)
+  }
+
   render() {
     return (
       <div>
         <h1>Hello {this.props.cityName}</h1>
+        <p>
+          {this.props.cityBlob && JSON.stringify(this.props.cityBlob.data[0].id)}
+        </p>
         <div>
           {this.props.cityBlob &&
-            this.props.cityBlob.data.map(activity => (
-              <div>
-                <img
-                  src={`https://res.cloudinary.com/thetripguru/image/upload/h_160,q_90,w_500/${
-                    activity.attributes.media.banners[0].src
-                  }.jpg`}
-                />
-                <p>{activity.attributes.title}</p>
-              </div>
+            this.props.cityBlob.data.map((activity, index) => (
+              <ActivityCard
+                imgSrc={activity.attributes.media.banners[0].src}
+                index={index}
+                title={activity.attributes.title}
+                onClick={event => this.handleActivityClick(event, activity.id)}
+              />
             ))}
         </div>
       </div>
     )
   }
+}
+
+const ActivityCard = props => {
+  const {imgSrc, index, title, ...otherProps} = props
+  return (
+    <div {...otherProps}>
+      <img
+        src={`https://res.cloudinary.com/thetripguru/image/upload/h_160,q_90,w_500/${imgSrc}.jpg`}
+      />
+      <p>
+        {index}: {title}
+      </p>
+    </div>
+  )
 }
